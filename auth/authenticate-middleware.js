6c2e7d3a456337933f42151641c09af6c377ed34
Ana Zoe <50473626+acarrillo3@users.noreply.github.com>
2020-02-28 11:52:28 -0500

    completed authentication middleware

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -2,7 +2,22 @@
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
+const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  const token = req.headers.authorization;
+  const secret = "this is a secret";
+
+  if (token) {
+    jwt.verify(token, secret, (err, token) => {
+      if (err) {
+        res.status(401).json({ you: "shall not pass!" });
+      } else {
+        req.token = token;
+        next();
+      }
+    });
+  } else {
+    res.status(401).json({ message: "Please try loggin in again!" });
+  }
 };
