a667b80665dd7bad304ec202cb724a4be4fb28bd
Ana Zoe <50473626+acarrillo3@users.noreply.github.com>
2020-02-28 11:50:02 -0500

    added token to logged in user

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const bcrypt = require("bcryptjs");
 const Users = require("../users/users-model");
+const jwt = require("jsonwebtoken");
 
 router.post("/register", (req, res) => {
   // implement registration
@@ -33,7 +34,8 @@ router.post("/login", (req, res) => {
       .first()
       .then(user => {
         if (user && bcrypt.compareSync(password, user.password)) {
-          res.status(200).json({ message: `Welcome ${username}` });
+          const token = assignToken(user);
+          res.status(200).json({ message: `Welcome ${username}`, token });
         } else {
           res.status(401).json({ message: "Invalid credentials!" });
         }
@@ -47,5 +49,17 @@ router.post("/login", (req, res) => {
     res.status(400).json({ message: "Please enter a username and password" });
   }
 });
+
+function assignToken(user) {
+  const payload = {
+    subject: user.id
+  };
+
+  const options = {
+    expiresIn: "1hr"
+  };
+
+  return jwt.sign(payload, "this is a secret", options);
+}
 
 module.exports = router;
